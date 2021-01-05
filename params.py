e5fe11cccdf5fd0d6c9874975e9fda6a32e1d9a4
Yousuke Utsumi <youtsumi@gmail.com>
2021-01-04 18:50:37 -0800

    add a feature to create variants for EO testing

---
+++ params.py
@@ -0,0 +1,165 @@
+params = [
+        {
+                "E2V": {
+                        "pl" : -6.0,
+                        "pswing" : 9.3,
+                        "sswing" : 9.3,
+                        "rgswing" : 10.1,
+                        "drd" : 8.0
+                },
+                "ITL": {
+                        "DAC": {
+                                "sclkHighP": 5.0,
+                                "sclkLowP": -5.5,
+                        },
+                        "Bias": {
+                                "ogP": -2.0+0.0
+                        }
+                },
+                "regex": "skeletons/regex.list",
+                "tag": "tmp"
+        },
+        {
+                "E2V": {
+                        "pl" : -6.0,
+                        "pswing" : 9.3,
+                        "sswing" : 9.3,
+                        "rgswing" : 10.1,
+                        "drd" : 8.0
+                },
+                "ITL": {
+                        "DAC": {
+                        },
+                        "Bias": {
+                        }
+                },
+                "regex": "skeletons/regex.list",
+                "tag": "1default"
+        },
+        {
+                "E2V": {
+                        "pl" : -6.0,
+                        "pswing" : 9.3,
+                        "sswing" : 9.3,
+                        "rgswing" : 10.1,
+                        "drd" : 8.0
+                },
+                "ITL": {
+                        "DAC": {
+                        },
+                        "Bias": {
+                        }
+                },
+                "regex": "skeletons/regex_csGate2.0.list",
+                "tag": "2csgate2.0"
+        },
+        {
+                "E2V": {
+                        "pl" : -6.0,
+                        "pswing" : 9.3,
+                        "sswing" : 9.3,
+                        "rgswing" : 10.5,
+                        "drd" : 8.0
+                },
+                "ITL": {
+                        "DAC": {
+                        },
+                        "Bias": {
+                        }
+                },
+                "regex": "skeletons/regex.list",
+                "tag": "3drg10.5"
+        },
+        {
+                "E2V": {
+                        "pl" : -6.0,
+                        "pswing" : 9.3,
+                        "sswing" : 9.5,
+                        "rgswing" : 10.1,
+                        "drd" : 8.0
+                },
+                "ITL": {
+                        "DAC": {
+                        },
+                        "Bias": {
+                        }
+                },
+                "regex": "skeletons/regex.list",
+                "tag": "4ds9.5"
+        },
+        {
+                "E2V": {
+                        "pl" : -6.0,
+                        "pswing" : 9.3,
+                        "sswing" : 9.3,
+                        "rgswing" : 10.1,
+                        "drd" : 8.0
+                },
+                "ITL": {
+                        "DAC": {
+                                "sclkHighP": 5.0,
+                                "sclkLowP": -5.5,
+                        },
+                        "Bias": {
+                        }
+                },
+                "regex": "skeletons/regex.list",
+                "tag": "5sl-5sh5.5"
+        },
+        {
+                "E2V": {
+                        "pl" : -6.0,
+                        "pswing" : 9.3,
+                        "sswing" : 9.3,
+                        "rgswing" : 10.1,
+                        "drd" : 8.0
+                },
+                "ITL": {
+                        "DAC": {
+                                "sclkHighP": 5.0,
+                                "sclkLowP": -6.0,
+                        },
+                        "Bias": {
+                        }
+                },
+                "regex": "skeletons/regex.list",
+                "tag": "6sl-5sh6"
+        },
+        {
+                "E2V": {
+                        "pl" : -6.0,
+                        "pswing" : 9.3,
+                        "sswing" : 9.3,
+                        "rgswing" : 10.1,
+                        "drd" : 8.0
+                },
+                "ITL": {
+                        "DAC": {
+                        },
+                        "Bias": {
+                                "ogP": -2.0+0.5
+                        }
+                },
+                "regex": "skeletons/regex.list",
+                "tag": "7og0.5"
+        },
+        {
+                "E2V": {
+                        "pl" : -6.0,
+                        "pswing" : 9.3,
+                        "sswing" : 9.3,
+                        "rgswing" : 10.1,
+                        "drd" : 8.0
+                },
+                "ITL": {
+                        "DAC": {
+                        },
+                        "Bias": {
+                                "ogP": -2.0-0.5
+                        }
+                },
+                "regex": "skeletons/regex.list",
+                "tag": "8og-0.5"
+        },
+
+]
